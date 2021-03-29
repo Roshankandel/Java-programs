77e6b72abdae312613e53fee7e99dc039a06f29e
Roshan kandel <62611761+Roshankandel@users.noreply.github.com>
2021-03-29 08:42:57 +0545

    Update and rename Javalab3.java to SystemExceptions.java

--- ExceptionHandling/javalab3/SystemExceptions.java
+++ ExceptionHandling/javalab3/SystemExceptions.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javalab3;
+//Program to demonstrate system Exception:
+package systemexceptions;
+public class SystemExceptions {
 
-/**
- *
- * @author DeLL
- */
-public class Javalab3 {
-
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         // TODO code application logic here
         int a,b,c;
